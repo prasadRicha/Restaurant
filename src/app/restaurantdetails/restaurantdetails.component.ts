09cbefdd3a5b3eb24f2ec7ec2939aa72ced4f36f
prasadRicha <41574728+prasadRicha@users.noreply.github.com>
2018-07-23 17:52:16 +0530

    commit for chnages done

--- src/app/restaurantdetails/restaurantdetails.component.ts
+++ src/app/restaurantdetails/restaurantdetails.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-restaurantdetails',
   templateUrl: './restaurantdetails.component.html',
-  styleUrls: ['./restaurantdetails.component.css']
 })
 export class RestaurantdetailsComponent implements OnInit {
 
